2415bc2e422f45021a9dce63196316ab829c8fe9
aakashlal601-web <aakashlal601@gmail.com>
2026-08-24 12:33:04 +0530

    Delete app directory

--- app/admin/(panel)/settings/page.js
+++
@@ -1,9 +0,0 @@
-import { prisma } from '../../../../lib/prisma';
-import SettingsClient from '../../../../components/admin/SettingsClient';
-
-export const dynamic = 'force-dynamic';
-
-export default async function SettingsPage() {
-  const settings = await prisma.storeSettings.upsert({ where: { id: 1 }, update: {}, create: { id: 1 } });
-  return <SettingsClient initialSettings={settings} />;
-}
